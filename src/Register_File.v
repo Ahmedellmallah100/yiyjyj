bf9fe92434f44130037a165b7f999af5e7c2a34c
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 21:13:43 +0300

    Update Register_File.v

--- src/Register_File.v
+++ src/Register_File.v
@@ -2,11 +2,11 @@ module Register_File (
     input         clk,
     input         areset,
 
-    input  [4:0]  A1,
-    input  [4:0]  A2,
-    input  [4:0]  A3,
+    input  [3:0]  A1,
+    input  [3:0]  A2,
+    input  [3:0]  A3,
 
-    input  [4:0]  ReadRegAddr,
+    input  [3:0]  ReadRegAddr,
 
     input  [31:0] WD3,
 
@@ -17,30 +17,30 @@ module Register_File (
     output [31:0] ReadRegData
 );
 
-reg [31:0] mem [0:31];
+reg [31:0] mem [0:15];
 
 integer i;
 
 // Read ports
-assign RD1 = (A1 == 5'd0) ? 32'd0 : mem[A1];
+assign RD1 = (A1 == 4'd0) ? 32'd0 : mem[A1];
 
-assign RD2 = (A2 == 5'd0) ? 32'd0 : mem[A2];
+assign RD2 = (A2 == 4'd0) ? 32'd0 : mem[A2];
 
 // External read port
 assign ReadRegData =
-    (ReadRegAddr == 5'd0) ? 32'd0 : mem[ReadRegAddr];
+    (ReadRegAddr == 4'd0) ? 32'd0 : mem[ReadRegAddr];
 
 // Write port
 always @(posedge clk) begin
 
     if (!areset) begin
 
-        for (i = 0; i < 32; i = i + 1)
+        for (i = 0; i < 16; i = i + 1)
             mem[i] <= 32'd0;
 
     end
 
-    else if (WE3 && (A3 != 5'd0)) begin
+    else if (WE3 && (A3 != 4'd0)) begin
 
         mem[A3] <= WD3;
 
